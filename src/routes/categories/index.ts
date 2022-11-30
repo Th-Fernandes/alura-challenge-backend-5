90c136d576314c3a9d745488b264435158d4e165
Thiago Fernandes <tjfctigo@gmail.com>
2022-11-30 06:35:04 -0300

    router GET:videos by categoryId

--- src/routes/categories/index.ts
+++ src/routes/categories/index.ts
@@ -87,4 +87,14 @@ router.delete("/:id", async ( req:Request, res:Response) => {
 
 })
 
+router.get("/:id/videos", async ( req:Request, res:Response ) => {
+  const { id } = req.params;
+  
+  const videosByCategoryId = await prisma.video.findMany({
+    where: { categoryId: Number(id) }
+  })
+
+  return res.status(200).json(videosByCategoryId)
+})
+
 export default router;
