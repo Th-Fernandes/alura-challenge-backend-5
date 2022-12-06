bff5e57dc20574b03970ede7d8de5a4e3d4b7a5a
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-12-06 09:22:36 -0300

    created: /signUp route

---
+++ src/routes/signUp.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
+
+const router = express.Router();
+
+router.post("/", async (req:Request, res:Response) => {
+  const { username, password } = req.body;
+  let createUser;
+
+  try {
+    createUser = await prisma.user.create({
+      data: { username, password }
+    })  
+  } catch(error) {
+    return res.json(error)
+  }
+
+  return res.status(201).json(createUser);
+})
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import express from "express";
 import videos from "./routes/videos";
 import categories from "./routes/categories";
 import signIn from "./routes/signIn"
+import signUp from "./routes/signUp"
 
 const app = express();
 app.use(express.json());
 
 app.use("/videos", videos);
 app.use("/categories/", categories);
-app.use("/signIn", signIn)
+app.use("/signIn", signIn);
+app.use("/signUp", signUp )
 
 export default app; 
