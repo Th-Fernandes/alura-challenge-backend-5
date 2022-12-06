d03397dcc30a96b1654b5850ce56522302f1b76a
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-12-06 09:47:47 -0300

    modified: /signIn route verification

--- src/routes/signIn.ts
+++ src/routes/signIn.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
 
 const router = express.Router();
 
-router.post("/", (req:Request, res:Response) => {
-  return res.json({message: 'logged in'})
+router.post("/", async (req:Request, res:Response) => {
+  const { username, password } = req.body;
+  let userVerify;
+
+  try 
+  {
+    userVerify = await prisma.user.findFirstOrThrow({
+      where: { username, password }
+    })
+  }
+  catch(error) 
+  {
+    res.json(error)
+  }
+
+  return res.json({status: 'ok', ...userVerify})
 })
 
 export default router;
