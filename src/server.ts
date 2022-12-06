8b0287698b40696a2b5151383508739a71fe6112
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-12-06 09:15:58 -0300

    created: users table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+
 import videos from "./routes/videos";
 import categories from "./routes/categories";
+import signIn from "./routes/signIn"
 
 const app = express();
 app.use(express.json());
 
 app.use("/videos", videos);
 app.use("/categories/", categories);
+app.use("/signIn", signIn)
 
 export default app; 
